6f8f33de983048542256ae724a2c1acff5557fb1
junbok97 <junbok97@gmail.com>
2023-04-03 13:56:18 +0900

    first commit

---
+++ Today Diary/Presentation/CreateViewController/CreateViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CreateViewModel.swift
+//  Today Diary
+//
+//  Created by 이준복 on 2023/04/03.
+//
+
+import Foundation
---
+++ Today Diary/Presentation/DetailDiaryViewController/DetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewModel.swift
+//  Today Diary
+//
+//  Created by 이준복 on 2023/04/03.
+//
+
+import Foundation
